ed0bd89a6c738008b7ec3824a58b601e5c967968
pierrelepottier <pierre@lepottier.fr>
2024-04-10 17:34:33 +0200

    fetch service

---
+++ museum_in_your_area_front/services/FetchAPI.js
@@ -0,0 +1,81 @@
+import axios from 'axios'
+
+export async function fetchMuseums(selectedFilters) {
+  let url = 'http://10.3.221.190:3000/api/v1/museums/filter'
+
+  const filtersActivated = selectedFilters.some((filter) => filter.length > 0)
+
+  if (filtersActivated) {
+    url += '?'
+    const filterTypes = ['cityId', 'departmentId', 'regionId', 'themeId']
+    let isFirstFilter = true
+
+    filterTypes.forEach((type, typeIndex) => {
+      if (selectedFilters[typeIndex].length > 0) {
+        if (!isFirstFilter) url += '&'
+        isFirstFilter = false
+        url += `${type}=`
+        selectedFilters[typeIndex].forEach((item, filterIndex) => {
+          url += item
+          if (filterIndex !== selectedFilters[typeIndex].length - 1) url += ','
+        })
+      }
+    })
+  }
+
+  try {
+    const response = await axios.get(url)
+    return response.data
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données :', error)
+    throw error
+  }
+}
+
+export async function fetchCities() {
+  let url = 'http://10.3.221.190:3000/api/v1/cities'
+
+  try {
+    const response = await axios.get(url)
+    return response.data
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données :', error)
+    throw error
+  }
+}
+
+export async function fetchRegions() {
+  let url = 'http://10.3.221.190:3000/api/v1/regions'
+
+  try {
+    const response = await axios.get(url)
+    return response.data
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données :', error)
+    throw error
+  }
+}
+
+export async function fetchDepartments() {
+  let url = 'http://10.3.221.190:3000/api/v1/departments'
+
+  try {
+    const response = await axios.get(url)
+    return response.data
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données :', error)
+    throw error
+  }
+}
+
+export async function fetchThemes() {
+  let url = 'http://10.3.221.190:3000/api/v1/themes'
+
+  try {
+    const response = await axios.get(url)
+    return response.data
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données :', error)
+    throw error
+  }
+}
