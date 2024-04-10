e1e58166364fcd342337cad051aa77595456b3d3
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-04-10 15:41:26 +0200

    FEAT add markers on map

--- museum_in_your_area_front/src/components/MapBoxComponent.vue
+++ museum_in_your_area_front/src/components/MapBoxComponent.vue
@@ -5,13 +5,43 @@ import { ref } from 'vue'
 mapboxgl.accessToken =
   'pk.eyJ1Ijoic3lsdmFpbmdhbHRpZXIiLCJhIjoiY2tsZ3JoZ3kyMWV3OTJ3cDdrcjM0azh0eiJ9.zH81EkDqnNnXFigXe1f7PQ'
 
-let museumMap
+let museumMap;
+
+const datas = [
+    {
+        "name": "Musée 1",
+        "coordonnees": "48.581229, 7.751984"
+    },
+    {
+        "name": "Musée 2",
+        "coordonnees": "45.321359, 0.586014"
+    },
+    {
+        "name": "Musée 3",
+        "coordonnees": "47.998314, 2.729233"
+    },
+    {
+        "name": "Musée 4",
+        "coordonnees": "44.79958240429276, 1.6175266643276274"
+    },
+]
 
 export default {
   mounted() {
+    // Ajouter le style de MapBox
+    const cssLink = document.createElement('link')
+    cssLink.href = 'https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css'
+    cssLink.rel = 'stylesheet'
+    document.head.appendChild(cssLink)
+
+    // Ajouter le script de MapBox
+    const script = document.createElement('script')
+    script.src = 'https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.js'
+    document.head.appendChild(script)
+
     const map = new mapboxgl.Map({
       container: this.$refs.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v12', // Replace with your preferred map style
+      style: 'mapbox://styles/mapbox/streets-v12',
       center: [-0.57918, 44.837789],
       zoom: 10
     })
@@ -34,9 +64,25 @@ async function coordonnees(pos) {
   museumMap.flyTo({
     center: userCoordinates
   })
+
+  placeMarkers(datas);
+}
+
+function placeMarkers(datas) {
+
+    datas.forEach((data) => {
+        const coords = data.coordonnees.split(', ');
+        new mapboxgl.Marker().setLngLat([coords[1], coords[0]]).addTo(museumMap)
+    });
+
+//   // Create a default Marker, colored black, rotated 45 degrees.
+//   const marker2 = new mapboxgl.Marker({ color: 'black', rotation: 45 })
+//     .setLngLat([12.65147, 55.608166])
+//     .addTo(museumMap)
 }
 
 navigator.geolocation.getCurrentPosition(coordonnees)
+
 </script>
 
 <template>
@@ -46,5 +92,6 @@ navigator.geolocation.getCurrentPosition(coordonnees)
 <style scoped>
 .map-container {
   height: 78.5vh;
+  flex: 1;
 }
 </style>
