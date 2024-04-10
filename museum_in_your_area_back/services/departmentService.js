0e18a97497b5098b332ae4fb59941fa59a4becb4
SyraxTarg <maimiti.saint-marc@efrei.net>
2024-04-10 17:36:07 +0200

    modification des noms department et city

--- museum_in_your_area_back/services/departmentService.js
+++ museum_in_your_area_back/services/departmentService.js
@@ -3,7 +3,18 @@ const { Department } = require("../models");
 
 async function getDepartments(){
     try {
-        return await Department.findAll();
+        const departments = await Department.findAll();
+        
+        // Modifier la clé "cityname" en "name" et supprimer la clé "cityname" pour chaque objet de la liste des villes
+        const modifiedDepartments = departments.map(department => {
+            const { department_name, ...rest } = department.toJSON(); // Utiliser la syntaxe de décomposition pour extraire "city_name" et le reste des propriétés
+            return {
+                ...rest, // Copier toutes les autres propriétés de la ville
+                name: department_name, // Utiliser "city_name" comme "name"
+            };
+        });
+
+        return modifiedDepartments;
       } catch (error) {
         console.error("Error fetching departments:", error);
         throw error;
