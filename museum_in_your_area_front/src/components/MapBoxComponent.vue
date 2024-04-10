0f3d1c29414764afb4a04687f97b889899c1615a
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-04-10 12:20:00 +0200

    ADD MapBox map

--- museum_in_your_area_front/src/components/MapBoxComponent.vue
+++ museum_in_your_area_front/src/components/MapBoxComponent.vue
@@ -1,7 +1,24 @@
-<script setup>
-
-</script>
-
 <template>
-    <p>MapBoxComponent</p>
+  <p>MapBoxComponent</p>
+
+  <div ref="mapContainer" id="map-container" class="map-container"></div>
 </template>
+<script>
+import mapboxgl from 'mapbox-gl'
+
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoic3lsdmFpbmdhbHRpZXIiLCJhIjoiY2tsZ3JoZ3kyMWV3OTJ3cDdrcjM0azh0eiJ9.zH81EkDqnNnXFigXe1f7PQ'
+export default {
+  mounted() {
+    const map = new mapboxgl.Map({
+      container: this.$refs.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v12', // Replace with your preferred map style
+      center: [-71.224518, 42.213995],
+      zoom: 9
+    })
+
+    this.map = map
+  }
+}
+</script>
