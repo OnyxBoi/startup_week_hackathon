6b7fe72bc5b2b8122ecdc2e1da07dcd4bee20f5d
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-04-10 15:17:17 +0200

    FEAT get user coordinates + fly to coordinates

--- museum_in_your_area_front/src/components/MapBoxComponent.vue
+++ museum_in_your_area_front/src/components/MapBoxComponent.vue
@@ -1,24 +1,50 @@
-<template>
-  <p>MapBoxComponent</p>
-
-  <div ref="mapContainer" id="map-container" class="map-container"></div>
-</template>
 <script>
 import mapboxgl from 'mapbox-gl'
-
+import { ref } from 'vue'
 
 mapboxgl.accessToken =
   'pk.eyJ1Ijoic3lsdmFpbmdhbHRpZXIiLCJhIjoiY2tsZ3JoZ3kyMWV3OTJ3cDdrcjM0azh0eiJ9.zH81EkDqnNnXFigXe1f7PQ'
+
+let museumMap
+
 export default {
   mounted() {
     const map = new mapboxgl.Map({
       container: this.$refs.mapContainer,
       style: 'mapbox://styles/mapbox/streets-v12', // Replace with your preferred map style
-      center: [-71.224518, 42.213995],
-      zoom: 9
+      center: [-0.57918, 44.837789],
+      zoom: 10
     })
 
-    this.map = map
+    museumMap = map
   }
 }
+
+let latitude = ref('')
+let longitude = ref('')
+
+async function coordonnees(pos) {
+  let crd = pos.coords
+
+  latitude.value = crd.latitude
+  longitude.value = crd.longitude
+
+  let userCoordinates = [longitude.value, latitude.value]
+
+  museumMap.flyTo({
+    center: userCoordinates
+  })
+}
+
+navigator.geolocation.getCurrentPosition(coordonnees)
 </script>
+
+<template>
+  <div ref="mapContainer" id="map-container" class="map-container"></div>
+</template>
+
+<style scoped>
+.map-container {
+  height: 78.5vh;
+}
+</style>
