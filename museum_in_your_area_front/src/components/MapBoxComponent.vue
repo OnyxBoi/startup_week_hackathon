608d71e38611d9e847a80404e5f180bea40a2e74
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-04-10 16:24:17 +0200

    FEAT popup on click on markers on map

--- museum_in_your_area_front/src/components/MapBoxComponent.vue
+++ museum_in_your_area_front/src/components/MapBoxComponent.vue
@@ -5,25 +5,26 @@ import { ref } from 'vue'
 mapboxgl.accessToken =
   'pk.eyJ1Ijoic3lsdmFpbmdhbHRpZXIiLCJhIjoiY2tsZ3JoZ3kyMWV3OTJ3cDdrcjM0azh0eiJ9.zH81EkDqnNnXFigXe1f7PQ'
 
-let museumMap;
+let museumMap
+let mapMarkers = ref([])
 
 const datas = [
-    {
-        "name": "Musée 1",
-        "coordonnees": "48.581229, 7.751984"
-    },
-    {
-        "name": "Musée 2",
-        "coordonnees": "45.321359, 0.586014"
-    },
-    {
-        "name": "Musée 3",
-        "coordonnees": "47.998314, 2.729233"
-    },
-    {
-        "name": "Musée 4",
-        "coordonnees": "44.79958240429276, 1.6175266643276274"
-    },
+  {
+    name: 'Musée 1',
+    coordonnees: '48.581229, 7.751984'
+  },
+  {
+    name: 'Musée 2',
+    coordonnees: '45.321359, 0.586014'
+  },
+  {
+    name: 'Musée 3',
+    coordonnees: '47.998314, 2.729233'
+  },
+  {
+    name: 'Musée 4',
+    coordonnees: '44.79958240429276, 1.6175266643276274'
+  }
 ]
 
 export default {
@@ -65,24 +66,39 @@ async function coordonnees(pos) {
     center: userCoordinates
   })
 
-  placeMarkers(datas);
+  placeMarkers(datas)
 }
 
 function placeMarkers(datas) {
+  datas.forEach((data) => {
+    const name = data.name
+    const innerHtmlContent = `<div style="min-width: 600px;font-size: large;color : black;">
+            <h4 class="h4Class">${name} </h4> 
+        </div>`
 
-    datas.forEach((data) => {
-        const coords = data.coordonnees.split(', ');
-        new mapboxgl.Marker().setLngLat([coords[1], coords[0]]).addTo(museumMap)
-    });
+    const divElement = document.createElement('div')
+    const assignBtn = document.createElement('div')
+    assignBtn.innerHTML = `<button class="btn btn-success btn-simple text-white" > Voir le musée </button>`
+    divElement.innerHTML = innerHtmlContent
+    divElement.appendChild(assignBtn)
+    assignBtn.addEventListener('click', () => {
+      console.log('Button clicked : ' + name)
+    })
 
-//   // Create a default Marker, colored black, rotated 45 degrees.
-//   const marker2 = new mapboxgl.Marker({ color: 'black', rotation: 45 })
-//     .setLngLat([12.65147, 55.608166])
-//     .addTo(museumMap)
+    const popup = new mapboxgl.Popup({
+      offset: 25
+    }).setDOMContent(divElement)
+
+    const coords = data.coordonnees.split(', ')
+    const marker = new mapboxgl.Marker()
+      .setLngLat([coords[1], coords[0]])
+      .setPopup(popup)
+      .addTo(museumMap)
+    mapMarkers.value.push(marker)
+  })
 }
 
 navigator.geolocation.getCurrentPosition(coordonnees)
-
 </script>
 
 <template>
