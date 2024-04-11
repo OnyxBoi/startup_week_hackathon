e3885dfd5c8bbaa9de12ee4e3a7acabe26654d71
OnyxBoi <152607901+OnyxBoi@users.noreply.github.com>
2024-04-11 13:51:46 +0200

    Added Distance Slider

--- museum_in_your_area_front/src/views/MapSearch.vue
+++ museum_in_your_area_front/src/views/MapSearch.vue
@@ -1,8 +1,10 @@
 <script setup>
 import MapBoxComponent from '@/components/MapBoxComponent.vue';
+import DistanceSlider from '@/components/DistanceSlider.vue';
 
 </script>
 
 <template>
+    <DistanceSlider />
     <MapBoxComponent />
 </template>
